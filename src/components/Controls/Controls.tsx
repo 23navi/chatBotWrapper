8f5495ff50d1a8e9cf37143408c46c50e2f44ae1
23navi <navisureka23@gmail.com>
2025-05-24 14:40:30 +0530

    handle chat controls

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -1,16 +1,32 @@
 import styles from "./Controls.module.css";
 import SendIcon from "../../icons/sendIcon";
+import { useState } from "react";
 
-export function Controls() {
+export function Controls({ onSend }: { onSend: (content: string) => void }) {
+  const [content, setContent] = useState("");
+
+  function handleContentChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    const content = event.target.value;
+    setContent(content);
+  }
+
+  function handleContentSend() {
+    if (content.trim() === "") return;
+    onSend(content);
+    setContent("");
+  }
+
   return (
     <div className={styles.Controls}>
       <div className={styles.TextAreaContainer}>
         <textarea
           className={styles.TextArea}
           placeholder="Message AI Chatbot"
+          value={content}
+          onChange={handleContentChange}
         />
       </div>
-      <button className={styles.Button}>
+      <button className={styles.Button} onClick={handleContentSend}>
         <SendIcon />
       </button>
     </div>
